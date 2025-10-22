9250d3c989bcb5330b8b7c7e5aacaf90fb721a66
Jhenie <highhalma@gmail.com>
2025-10-22 10:51:01 -0300

    finalizado

--- perfil-usuario/src/App.jsx
+++ perfil-usuario/src/App.jsx
@@ -6,41 +6,37 @@ function App() {
   const [user, setUsers] = useState([]);
   const [endereco, setEndereco] = useState("");
   const [habilidades, setHabilidades] = useState("");
+  const [projetos, setProjetos] = useState("");
 
   useEffect(() => {
     getUsers();
-    getEndereco(user);
-    getHabilidades(user);
   }, []);
 
   const getUsers = async () => {
     const response = await axios.get("https://avaliacao-bosch.onrender.com/usuario");
     setUsers(response.data);
+    setEndereco(response.data.endereco);
+    setHabilidades(response.data.habilidades);
+    setProjetos(response.data.projetos);
   }
-
-  const getEndereco = (user) => {
-    setEndereco(`${user.endereco.rua}, ${user.endereco.numero} — ${user.endereco.cidade}, ${user.endereco.estado}`)
-  }
-
-  const getHabilidades = (user) => {
-    setHabilidades(user.habilidades[0], user.habilidades[1], user.habilidades[2], user.habilidades[3])
-  }
-
-  console.log(`${user.endereco.rua}, ${user.endereco.numero} — ${user.endereco.cidade}, ${user.endereco.estado}`)
-  console.log(user.habilidades[0], user.habilidades[1], user.habilidades[2], user.habilidades[3])
 
   return (
     <>
-      <div className="flex justify-center flex-row bg-gray-200 p-7">
+      <div className="flex justify-around flex-row bg-gray-200 p-7 w-250">
         <div className="flex flex-col text-left">
-          <h2 className="mt-5 mb-5 text-6xl font-bold">{user.nome}</h2>
+          <h2 className="mt-5 mb-6 text-6xl font-bold">{user.nome}</h2>
           <p className="mb-2 text-2xl"><strong>Idade:</strong> {user.idade}</p>
           <p className="mb-2 text-2xl"><strong>Email:</strong> {user.email}</p>
-          <p className="mb-2 text-2xl"><strong>Endereço:</strong> {endereco}</p>
-          <p className="mb-2 text-2xl"><strong>Stacks: </strong> {habilidades}</p>
+          <p className="mb-2 text-2xl"><strong>Endereço:</strong> {endereco.rua}, {endereco.numero} <br />
+            {endereco.cidade} — {endereco.estado}</p>
+          <p className="mb-2 text-2xl"><strong>Stacks: </strong> {habilidades[0]}, {habilidades[1]}<br />
+            {habilidades[2]}, {habilidades[3]}</p>
         </div>
-        <div className="flex flex-col text-left">
-          <h2 className="ml-10 mt-5 mb-5 text-5xl font-bold">Projetos</h2>
+        <div className="flex flex-col text-left ml-20">
+          <h2 className="mt-5 mb-6 text-5xl font-bold">Projetos</h2>
+          <p className="mb-2 text-2xl"><strong>{projetos[0].id}. {projetos[0].nome}</strong><br />Status: {projetos[0].status}</p>
+          <p className="mb-2 text-2xl"><strong>{projetos[1].id}. {projetos[1].nome}</strong><br />Status: {projetos[1].status}</p>
+          <p className="mb-2 text-2xl"><strong>{projetos[2].id}. {projetos[2].nome}</strong><br />Status: {projetos[2].status}</p>
         </div>
       </div>
     </>
